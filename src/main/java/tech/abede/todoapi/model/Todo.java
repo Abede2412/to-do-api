71bc5ce63071abf71956b368cb15ed5ea31c3e32
abede <abdulrahman241296@gmail.com>
2023-05-05 15:28:04 +0800

    feat: create model

---
+++ src/main/java/tech/abede/todoapi/model/Todo.java
@@ -0,0 +1,30 @@
+package tech.abede.todoapi.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String description;
+    private Boolean isFinish = false;
+
+    public void isFinishTrue(){
+        this.isFinish = true;
+    }
+}
