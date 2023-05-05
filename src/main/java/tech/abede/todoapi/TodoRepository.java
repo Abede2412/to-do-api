6545674abdd8fe87c7f347cd195fbc71e7fbde3a
abede <abdulrahman241296@gmail.com>
2023-05-05 15:47:07 +0800

    feat: add TodoRepository.java

---
+++ src/main/java/tech/abede/todoapi/TodoRepository.java
@@ -0,0 +1,10 @@
+package tech.abede.todoapi;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TodoRepository extends JpaRepository<Todo, Integer>{
+    
+
+ 
+    
+}
