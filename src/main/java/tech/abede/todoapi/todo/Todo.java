17d236a215f1ef73e63747b9122b193dc4f51597
abede <abdulrahman241296@gmail.com>
2023-05-07 21:39:32 +0800

    sementara

--- src/main/java/tech/abede/todoapi/todo/Todo.java
+++ src/main/java/tech/abede/todoapi/todo/Todo.java
@@ -30,5 +30,4 @@ public class Todo {
     public void isFinishTrue(){
         this.isFinish = true;
     }
-
 }
--- src/main/java/tech/abede/todoapi/todo/TodoController.java
+++ src/main/java/tech/abede/todoapi/todo/TodoController.java
@@ -17,15 +17,14 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+@CrossOrigin(origins= "http://localhost:3000",allowedHeaders = "*")
 public class TodoController {
 
     private final TodoService todoService;
